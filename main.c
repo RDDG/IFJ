4c4c8deef9bdd156b5cc421c82f294f3f079320a
Pavel Dohnalik <p.dohnalik@centrum.cz>
2016-10-01 22:18:50 +0200

    upraveny main.c

--- main.c
+++ main.c
@@ -3,7 +3,17 @@
 
 int main (int argc, char *argv[])
 {
+    
+    if (argc==1 || argc>=3){
+        printf("Chyba neni zadan vstupni soubor\n");
+        return 99;
+    }
+    
     // Otevreni souboru ze vstupu a predani rizeni parseru, ktery vola funkce z lexikalu
+    FILE * f;
+    f=fopen(argv[1], "r");
+    
+    fclose(f);
     
     
     
