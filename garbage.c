849aaecac0ec2e7b25b335b87aa4f6c43aeb67ec
Pavel Dohnalik <p.dohnalik@centrum.cz>
2016-10-13 22:56:20 +0200

    garbage bez memory leak a erroru

--- garbage.c
+++ garbage.c
@@ -4,7 +4,7 @@
 struct GC gc;
 
 void* myMalloc(unsigned long velikost){
-    if(gc.posledni->polozka!=NULL){
+    if(gc.posledni->polozka!=NULL || gc.prvni!=gc.posledni){
         gc.posledni->dalsi=malloc(sizeof(struct GC_obsah));
         gc.posledni=gc.posledni->dalsi;
     }
@@ -17,7 +17,7 @@ void* myMalloc(unsigned long velikost){
 void init(){
     gc.prvni=malloc(sizeof(struct GC_obsah));
     gc.posledni=gc.prvni;
-    gc.prvni->dalsi=NULL;
+    gc.prvni->polozka=NULL;
 }
 
 void freeAll(){
@@ -33,7 +33,6 @@ void freeAll(){
         akt=akt->dalsi;
         free(tmp);
     }
-    gc.prvni=NULL;
     
 }
 
