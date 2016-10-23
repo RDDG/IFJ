66b78507326d83a35fc014ea99cd345f85da1720
Pavel Dohnalik <p.dohnalik@centrum.cz>
2016-10-23 21:59:12 +0200

    parser funkce LLclass

--- parser.c
+++ parser.c
@@ -1,20 +1,55 @@
 #include "parser.h"
 
 
-<N> => class <id> { <class_body> } <class_def>
+//<N> => class <id> { <class_body> } <class_def>
 int LLclass(){
     
+    int error =1;
     Token * token;
-    token=getToken()
-    if(token->type == )
+    
+    token=getToken();
+    if(token->type != t_class){
+        return error;
+    }
+    
+    token=getToken();
+    if(token->type != t_simple_id){
+        return error;
+    }
+    
+    token=getToken();
+    if(token->type != t_LSlozena){
+        return error;
+    }
+    
+    error=LLClassBody();
+    if( error == 1){
+        return error;
+    }
+    
+    token=getToken();
+    if (token->type != t_PSlozena){
+        return error;
+    }
+    
+    error=LLclassDef();
+    if( error == 1){
+        return error;
+    }
+    
+    return 0;
+    
 }
 
-void parseStart(){
-    int error = 0;
+int parseStart(){
+    int error = 1;
     
-    if(LLparserID(), LL
+    error=LLclass();
+    if (error == 0){
+        //spustit interpret s paskou na vstuput
+    }
     
-    Token *token;
+    return error;
     
     
     
